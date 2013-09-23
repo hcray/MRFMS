fce33ecc6cd340b13a636f3ccc5a1a33bf175669
caiyingying <caiyingying234@gmail.com>
2013-09-23 22:12:14 +0800

    安安

---
+++ src/com/krakentouch/action/BaseAction.java
@@ -0,0 +1,22 @@
+package com.krakentouch.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.apache.struts2.interceptor.ServletResponseAware;
+
+public class BaseAction implements ServletRequestAware,ServletResponseAware{
+
+	 HttpServletRequest request;
+	 HttpServletResponse response;
+	
+	public void setServletRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+
+	public void setServletResponse(HttpServletResponse response) {
+		this.response = response;
+	}
+
+}
